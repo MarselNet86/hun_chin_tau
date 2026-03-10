cef25903a7a6f3ffbbc5e4632ce72cf5f95fb74f
mars <schamsutdin88@gmail.com>
2026-03-10 19:49:07 +0500

    Исправили отображение интро в главном окне

--- hun_chin_tau/settings.py
+++ hun_chin_tau/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'anime/static',
+    BASE_DIR / 'static',
+]
 
 # Media files
 MEDIA_URL = 'media/'
